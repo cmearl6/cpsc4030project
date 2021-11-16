13f76fd97c0025e2cfabec4662d39f80bfcac4d5
Tyler Piel <44116860+tpiel@users.noreply.github.com>
2021-11-16 17:14:59 -0500

    Added axis and labels

--- scatter_main.js
+++ scatter_main.js
@@ -2,10 +2,10 @@
 d3.csv("dataset/player_attributes.csv").then(function (dataset) {
     var dimensions = {
         width: 1500,
-        height: 1200,
+        height: 1000,
         margin: {
             top: 50,
-            bottom: 250,
+            bottom: 100,
             right: 100,
             left: 150
         }
@@ -13,15 +13,17 @@ d3.csv("dataset/player_attributes.csv").then(function (dataset) {
     var svg = d3.select('#scatter')
         .style("width", dimensions.width)
         .style("height", dimensions.height)
+ //       .style("background-color", "")
 
     var yAccessor = d => +d.PTS
     var xAccessor = d => d.TEAM_ABBREVIATION
     var rAccessor = d => +d.SALARY
 
     var teams = ["ATL", "BOS", "CLE", "NOP", "CHI", "DAL", "DEN", "GSW", "HOU", "LAC", "LAL", "MIA", "MIL", "MIN", "BKN", "NYK", "ORL", "IND", "PHI", "PHX", "POR", "SAC", "SAS", "OKC", "TOR", "UTA", "MEM", "WAS", "DET", "CHA"]
-    var team_colors = ["#C8102E", "#007A33", "#860038", "#0C2340", "#CE1141", "#00538C", "#0E2240", "#FFC72C","#FFFFFF","#1D428A","#552583","#98002E","#00471B","#0C2340","#000000","#F58426","#0077C0","#FDBB30","#006BB6","#1D1160","#000000","#63727A","#C4CED4","#EF3B24","#CE1141","#002B5C","#5D76A9","#002B5C","#C8102E","#00788C"]
-    var team_colors2 = ["#FFCD00", "#BA9653", "#041E42", "#C8102E", "#000000", "#002B5E", "#FEC524", "#1D428A", "#CE1141", "#C8102E", "#FDB927", "#F9A01B", "#EEE1C6", "#236192", "#000000", "#006BB6", "#C4CED4", "#002D62", "#ED174C", "#E56020", "#E03A3E", "#5A2D81", "#000000", "#007AC1", "#000000", "#00471B", "#12173F", "#E31837", "#1D42BA","#1D1160"]
+    var team_colors = ["#C8102E", "#007A33", "#860038", "#0C2340", "#CE1141", "#00538C", "#0E2240", "#FFC72C","#FFFFFF","#1D428A","#552583","#98002E","#00471B","#0C2340","#000000","#F58426","#C4CED4","#FDBB30","#006BB6","#1D1160","#000000","#63727A","#C4CED4","#EF3B24","#CE1141","#002B5C","#5D76A9","#002B5C","#C8102E","#00788C"]
+    var team_colors2 = ["#FFCD00", "#BA9653", "#041E42", "#C8102E", "#000000", "#002B5E", "#FEC524", "#1D428A", "#CE1141", "#C8102E", "#FDB927", "#F9A01B", "#EEE1C6", "#236192", "#FFFFFF", "#006BB6", "#0077C0", "#002D62", "#ED174C", "#E56020", "#E03A3E", "#5A2D81", "#000000", "#007AC1", "#000000", "#00471B", "#12173F", "#E31837", "#1D42BA","#1D1160"]
 
+    
     var xScale = d3.scaleBand()
         .domain(teams)
         .range([dimensions.margin.left, dimensions.width - dimensions.margin.right])
@@ -54,7 +56,8 @@ d3.csv("dataset/player_attributes.csv").then(function (dataset) {
         .data(activeplayers)
         .enter()
         .append("circle")
-        .attr("cx", d => xScale(xAccessor(d)))
+        .attr("cx", d => xScale(xAccessor(d)) + 20)
+        // add 20 to get it lined up with the lines correctly
         .attr("cy", d => yScale(yAccessor(d)))
         .attr("fill", d => colorScale1(xAccessor(d)))
         .attr("opacity",0.7)
@@ -73,4 +76,19 @@ d3.csv("dataset/player_attributes.csv").then(function (dataset) {
         .attr("dx", "-.8em")
         .attr("dy", ".05em")
         .attr("transform", "rotate(-65)")
+
+    svg.append("text")
+        .attr("x", (dimensions.width - dimensions.margin.right) / 2)
+        .attr("y", dimensions.height - dimensions.margin.bottom + 50)
+        .style("text-anchor", "middle")
+        .text("Team")
+
+    svg.append('g')
+        .call(d3.axisLeft(yScale))
+        .style("transform", `translateX(${dimensions.margin.left}px)`)
+
+    svg.append("text")
+        .attr("transform", "translate(" + (dimensions.margin.left / 2) + "," + (dimensions.height / 2) + ")rotate(-90)")
+        .style("text-anchor", "middle")
+        .text("Points")
 })
