473e287686e3ae0e70eb7167885407e4ce22cdd8
Tyler Piel <44116860+tpiel@users.noreply.github.com>
2021-11-16 16:54:23 -0500

    Add stroke to scatter and start x axis

--- scatter_main.js
+++ scatter_main.js
@@ -2,10 +2,10 @@
 d3.csv("dataset/player_attributes.csv").then(function (dataset) {
     var dimensions = {
         width: 1500,
-        height: 1000,
+        height: 1200,
         margin: {
-            top: 10,
-            bottom: 50,
+            top: 50,
+            bottom: 250,
             right: 100,
             left: 150
         }
@@ -19,7 +19,8 @@ d3.csv("dataset/player_attributes.csv").then(function (dataset) {
     var rAccessor = d => +d.SALARY
 
     var teams = ["ATL", "BOS", "CLE", "NOP", "CHI", "DAL", "DEN", "GSW", "HOU", "LAC", "LAL", "MIA", "MIL", "MIN", "BKN", "NYK", "ORL", "IND", "PHI", "PHX", "POR", "SAC", "SAS", "OKC", "TOR", "UTA", "MEM", "WAS", "DET", "CHA"]
- //   var team_colors = ["#E03A3E"
+    var team_colors = ["#C8102E", "#007A33", "#860038", "#0C2340", "#CE1141", "#00538C", "#0E2240", "#FFC72C","#FFFFFF","#1D428A","#552583","#98002E","#00471B","#0C2340","#000000","#F58426","#0077C0","#FDBB30","#006BB6","#1D1160","#000000","#63727A","#C4CED4","#EF3B24","#CE1141","#002B5C","#5D76A9","#002B5C","#C8102E","#00788C"]
+    var team_colors2 = ["#FFCD00", "#BA9653", "#041E42", "#C8102E", "#000000", "#002B5E", "#FEC524", "#1D428A", "#CE1141", "#C8102E", "#FDB927", "#F9A01B", "#EEE1C6", "#236192", "#000000", "#006BB6", "#C4CED4", "#002D62", "#ED174C", "#E56020", "#E03A3E", "#5A2D81", "#000000", "#007AC1", "#000000", "#00471B", "#12173F", "#E31837", "#1D42BA","#1D1160"]
 
     var xScale = d3.scaleBand()
         .domain(teams)
@@ -34,6 +35,14 @@ d3.csv("dataset/player_attributes.csv").then(function (dataset) {
         .domain(d3.extent(dataset, rAccessor))
         .range([3, 30])
 
+    var colorScale1 = d3.scaleOrdinal()
+        .domain(teams)
+        .range(team_colors)
+
+    var colorScale2 = d3.scaleOrdinal()
+        .domain(teams)
+        .range(team_colors2)
+
     console.log(rScale(1))
 
     console.log(dataset)
@@ -47,8 +56,21 @@ d3.csv("dataset/player_attributes.csv").then(function (dataset) {
         .append("circle")
         .attr("cx", d => xScale(xAccessor(d)))
         .attr("cy", d => yScale(yAccessor(d)))
-        .attr("fill", "black")
-        .attr("opacity",0.4)
+        .attr("fill", d => colorScale1(xAccessor(d)))
+        .attr("opacity",0.7)
         .attr("r", d => rScale(rAccessor(d)))
+        .attr("stroke", d => colorScale2(xAccessor(d)))
+        .attr("stroke-width",3)
 
+    var xAxisgen = d3.axisBottom().scale(xScale)
+
+    svg.append('g')
+        .call(d3.axisBottom(xScale)
+            .tickValues(xScale.domain()))
+        .style("transform", `translateY(${dimensions.height - dimensions.margin.bottom}px)`)
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".05em")
+        .attr("transform", "rotate(-65)")
 })
